f398c9f811605f72fe52c3d685942cf506c6f694
your-storre <babrianalee@gmail.com>
2026-05-28 10:11:48 +0000

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    domains: ['images.unsplash.com', 'flagcdn.com', 'nyhiqzpzffkwzgnmjwnp.supabase.co'],
-  },
-  swcMinify: true,
-  reactStrictMode: true,
-}
